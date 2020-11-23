f261c033faecf0d3ed786863a4e55f1d83586359
Leon Thoma <leonthoma@gmx.de>
2020-11-23 14:18:30 +0100

    Rewrote jags code; renaming all ambiguous var names and matching the to the paper
    Null model running without issues

--- hallmann_repro.R
+++ hallmann_repro.R
@@ -1,15 +1,10 @@
 ## Hallmann 2017
 
-# required libraries
 library(R2jags)
 library(RCurl) # used for loading data from github
 
 # ---- Load & Standardize ----
-
-# Function to standardize variables
-standardize <- function(x, mu = mean(x), sig = sd(x)) (x-mu)/sig
-
-# read the data
+# Read the data
 url_1 <- getURL("https://raw.githubusercontent.com/leonthoma/Repro/master/Hallmann_s1_data.csv")
 url_2 <- getURL("https://raw.githubusercontent.com/leonthoma/Repro/master/Hallmann_s2_data.csv")
 
@@ -22,7 +17,7 @@ model.frame <- read.csv(text = url_2, header = TRUE, sep = ",")
 # ---- Null Model ----
 
 ## Indices contain the corresponding rows in model.frame of the  starting (1)
-## and end (2) position of the potID groups
+## and end (2) position of the potID groups; tau_1/2 in the paper
 ## plot is used to get plot info for the location-specific random effect (eps)
 ## loctype is used for the habitat cluster covariate
 
@@ -39,39 +34,40 @@ jagsdataNull <- list(
   nrandom = max(as.numeric(model.frame$plot))
 )
 
-sink("NullModel.jag")
-cat("
-model{
-## likelihood function for the latent expected daily biomass
-for (i in 1:n){
-m[i] ~ dnorm(sum(y[index1[i]:index2[i]]), tau[i])
-tau[i] <- 1 / Var[i]
-Var[i] <- sum(vr[index1[i]:index2[i]])
-}
-
-## likelihood function for muHat (y), it's dependent function (z) and variance (vr)
-for (i in 1:ndaily){
-y[i]  <- exp(z[i]) ## z is actually called y in the paper
-z[i]  <- int + c[1] * daynr[i] + c[2] * daynr2[i] + b[loctype[i]] + eps[plot[i]]
-## this is called y in the paper
-vr[i] <- exp(2 * z[i] + lvar) * (exp(lvar) - 1) ## variance
-}
-
-## priors
-int ~ dnorm(0,.01) ## global intercept (see eq. 4 in paper)
-b[1] <- 0  
-for (i in 2:3) {b[i] ~ dnorm(0, .01)}
-for (i in 1:2) {c[i] ~ dnorm(0, .01)}
-sdhat ~ dunif(0, 5)
-lvar <- pow(sdhat, 2) ## variance of daily log-biomass
-for(i in 1:nrandom) {
-eps[i] ~ dnorm(0, tau.re) ## location-specific random effect (see eq. 4 in paper)
-}
-tau.re <- pow(sd.re, -2) ## convert sd to percision parameter of normal dist
-sd.re ~ dunif(0, 1)
-}
-")
-sink(NULL)
+## NOT RUN
+# sink("NullModel.jag")
+# cat("
+# model{
+# ## likelihood function for the latent expected daily biomass
+# for (i in 1:n){
+# m[i] ~ dnorm(sum(y[index1[i]:index2[i]]), tau[i])
+# tau[i] <- 1 / Var[i] ## 1/variance: using precision parameter not Variance  
+# Var[i] <- sum(vr[index1[i]:index2[i]])
+# }
+# 
+# ## likelihood function for muHat (y), it's dependent function (z) and variance (vr)
+# for (i in 1:ndaily){
+# y[i]  <- exp(z[i]) ## z is actually called y in the paper
+# z[i]  <- int + c[1] * daynr[i] + c[2] * daynr2[i] + b[loctype[i]] + eps[plot[i]]
+# ## this is called y in the paper
+# vr[i] <- exp(2 * z[i] + lvar) * (exp(lvar) - 1) ## variance
+# }
+# 
+# ## priors
+# int ~ dnorm(0,.01) ## global intercept (see eq. 4 in paper)
+# b[1] <- 0  
+# for (i in 2:3) {b[i] ~ dnorm(0, .01)}
+# for (i in 1:2) {c[i] ~ dnorm(0, .01)}
+# sdhat ~ dunif(0, 5)
+# lvar <- pow(sdhat, 2) ## variance of daily log-biomass
+# for(i in 1:nrandom) {
+# eps[i] ~ dnorm(0, tau.re) ## location-specific random effect (see eq. 4 in paper)
+# }
+# tau.re <- pow(sd.re, -2) ## convert sd to precision parameter of normal dist
+# sd.re ~ dunif(0, 1)
+# }
+# ")
+# sink(NULL)
 
 # Run the model
 parametersNull <- c("int", "b", "c", "eps", "sdhat", "sd.re")
@@ -79,7 +75,6 @@ jagsmod0 <- jags(jagsdataNull, inits = NULL, parametersNull,
                 "NullModel.jag", n.chains = 3, n.iter = 120, n.burnin = 20,
                 n.thin = 10)
 jagsmod0
-
 
 # ---- Re-Analysis ----
 library(dplyr)
@@ -94,5 +89,61 @@ new_plots <- group_by(py, plot) %>%
  slice(1) # get first sampling year of each plot
 
 new_data <- semi_join(data, new_plots, by = c("plot", "year"))
+new_model.frame <- semi_join(model.frame, new_plots, by = c("plot", "year"))
 
+# New Null model (with meaningful/unambiguous variable names)
+newjagsdataNull <- list(
+  m_bio = new_data$biomass,
+  tau1 = with(new_model.frame, tapply(1:nrow(new_model.frame), potID, min)), 
+  tau2 = with(new_model.frame, tapply(1:nrow(new_model.frame), potID, max)),
+  plot = as.numeric(new_model.frame$plot),
+  loctype = as.numeric(new_data$location.type[match(new_model.frame$potID, new_data$potID)]),
+  daynr = as.numeric((new_model.frame$daynr - mean(new_data$mean.daynr)) / sd(new_data$mean.daynr)),
+  daynr2 = as.numeric((new_model.frame$daynr - mean(new_data$mean.daynr)) / sd(new_data$mean.daynr))^2,
+  ndaily = nrow(new_model.frame),
+  n = nrow(new_data),
+  nrandom = max(as.numeric(new_model.frame$plot))
+)
 
+# Jags model
+
+## NEEDS TO RUN ONCE TO CREATE .JAGS FILE
+## SET WD TO APROPRIATE DIRECTORY
+# sink("newNullModel.jag")
+# cat("model{
+# ## Likelihood function for the latent expected daily biomass
+# for (i in 1:n){
+# m_bio[i] ~ dnorm(sum(z[tau1[i]:tau2[i]]), sig_sq[i]) # measured daily biomass
+# sig_sq[i] <- 1 / Var[i] # Precision
+# Var[i] <- sum(vr[tau1[i]:tau2[i]]) # Variance of measured biomass
+# }
+# 
+# ## Likelihood function for muHat, it's dependent function and variance
+# for (i in 1:ndaily){
+# z[i]  <- exp(y[i]) # Latent expected daily biomass
+# y[i]  <- g_intcp + c[1] * daynr[i] + c[2] * daynr2[i] + b[loctype[i]] + eps[plot[i]]
+# vr[i] <- exp(2 * y[i] + lvar) * (exp(lvar) - 1) # Variance (see paper eq. 6)
+# }
+# 
+# ## Priors
+# g_intcp ~ dnorm(0,.01) # global intercept (see eq. 4 in paper [c])
+# b[1] <- 0 
+# for (i in 2:3) {b[i] ~ dnorm(0, .01)} # Habitat cluster 2 & 3 covariates
+# for (i in 1:2) {c[i] ~ dnorm(0, .01)} # coef for daynr & naynr^2 covariates
+# sdhat ~ dunif(0, 5) # Estimated sd
+# lvar <- pow(sdhat, 2) # Residual variance of daily log-biomass
+# for(i in 1:nrandom) {
+# eps[i] ~ dnorm(0, sig_sq.re) # Location-specific random effect (see eq. 4 in paper [u_s])
+# }
+# sig_sq.re <- pow(sd.re, -2) # Residual [sigma^2_j]; pow():Convert sd to percision parameter
+# sd.re ~ dunif(0, 1) # Residual sd
+# }
+# ")
+# sink(NULL)
+
+# Run the model
+parametersNull <- c("int", "b", "c", "eps", "sdhat", "sd.re")
+newjagsmod0 <- jags(newjagsdataNull, inits = NULL, parametersNull,
+                 "newNullModel.jag", n.chains = 3, n.iter = 120, n.burnin = 20,
+                 n.thin = 10)
+newjagsmod0
