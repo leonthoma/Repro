18c43557f13f77e301b6afb2a09394e142f35dba
leon thoma <leonthoma@gmx.de>
2021-01-16 17:23:16 +0100

    changed predict functions to work with both models; fill fy not working !

--- hallmann_repro.R
+++ hallmann_repro.R
@@ -105,7 +105,28 @@ oldjagsmodBasic <- jags(jagsdataBasic, inits = NULL, parametersBasic,
                         "BasicModel.jag", n.iter = 12000, n.burnin = 2000, 
                         n.chains = 3, n.thin = 10)
 
-old_parms_mean <- oldjagsmodBasic$BUGSoutput$mean
+# Get mean parameter values from posterior
+parms_mean <- oldjagsmodBasic$BUGSoutput$mean 
+
+# 2.4 % credible interval
+parms_mean_lo <- list("b" = as.numeric(oldjagsmodBasic$BUGSoutput$summary[1:3, 3]),
+                          "c" = as.numeric(oldjagsmodBasic$BUGSoutput$summary[4:7, 3]),
+                          "deviance" = oldjagsmodBasic$BUGSoutput$summary[8, 3],
+                          "eps" = as.numeric(oldjagsmodBasic$BUGSoutput$summary[9:71, 3]),
+                          "g_intcp" = oldjagsmodBasic$BUGSoutput$summary[72, 3],
+                          "log.lambda" = oldjagsmodBasic$BUGSoutput$summary[73, 3],
+                          "sd.re" = oldjagsmodBasic$BUGSoutput$summary[74, 3],
+                          "sdhat" = oldjagsmodBasic$BUGSoutput$summary[75, 3])
+
+# 97.5 % credible interval
+parms_mean_hi <- list("b" = as.numeric(oldjagsmodBasic$BUGSoutput$summary[1:3, 7]),
+                          "c" = as.numeric(oldjagsmodBasic$BUGSoutput$summary[4:7, 7]),
+                          "deviance" = oldjagsmodBasic$BUGSoutput$summary[8, 7],
+                          "eps" = as.numeric(oldjagsmodBasic$BUGSoutput$summary[9:71, 7]),
+                          "g_intcp" = oldjagsmodBasic$BUGSoutput$summary[72, 7],
+                          "log.lambda" = oldjagsmodBasic$BUGSoutput$summary[73, 7],
+                          "sd.re" = oldjagsmodBasic$BUGSoutput$summary[74, 7],
+                          "sdhat" = oldjagsmodBasic$BUGSoutput$summary[75, 7])
 
 # ---- Re-Analysis ----
   # ---- Data wrangling ----
@@ -246,18 +267,13 @@ jagsmodBasic <- jags(newjagsdataBasic, inits = NULL, parametersBasic,
 jagsmodBasic
 
 # ---- Diagnostics ----
-  # Trace plot
-  traceplot(jagsmodBasic)
-
-  # Gelman
-  gelman.diag(jagsmodBasic)
 
 ## ---- Calculate predicted values ----
 # Get mean parameter values from posterior
-parms_mean <- jagsmodBasic$BUGSoutput$mean 
+new_parms_mean <- jagsmodBasic$BUGSoutput$mean 
 
 # 2.4 % credible interval
-parms_mean_lo <- list("b" = as.numeric(jagsmodBasic$BUGSoutput$summary[1:3, 3]),
+new_parms_mean_lo <- list("b" = as.numeric(jagsmodBasic$BUGSoutput$summary[1:3, 3]),
                       "c" = as.numeric(jagsmodBasic$BUGSoutput$summary[4:7, 3]),
                       "deviance" = jagsmodBasic$BUGSoutput$summary[8, 3],
                       "eps" = as.numeric(jagsmodBasic$BUGSoutput$summary[9:71, 3]),
@@ -267,7 +283,7 @@ parms_mean_lo <- list("b" = as.numeric(jagsmodBasic$BUGSoutput$summary[1:3, 3]),
                       "sdhat" = jagsmodBasic$BUGSoutput$summary[75, 3])
 
 # 97.5 % credible interval
-parms_mean_hi <- list("b" = as.numeric(jagsmodBasic$BUGSoutput$summary[1:3, 7]),
+new_parms_mean_hi <- list("b" = as.numeric(jagsmodBasic$BUGSoutput$summary[1:3, 7]),
                        "c" = as.numeric(jagsmodBasic$BUGSoutput$summary[4:7, 7]),
                        "deviance" = jagsmodBasic$BUGSoutput$summary[8, 7],
                        "eps" = as.numeric(jagsmodBasic$BUGSoutput$summary[9:71, 7]),
@@ -278,89 +294,123 @@ parms_mean_hi <- list("b" = as.numeric(jagsmodBasic$BUGSoutput$summary[1:3, 7]),
   
   
 # Set tau & intervals of days
-t_1 <- as.vector(newjagsdataBasic$tau1[1:nrow(new_data)])
-t_2 <- as.vector(newjagsdataBasic$tau2[1:nrow(new_data)])
-ints <- map(1:nrow(new_data), ~ seq(t_1[.x], t_2[.x]))
+t_1 <- as.vector(jagsdataBasic$tau1[1:nrow(data)])
+t_2 <- as.vector(jagsdataBasic$tau2[1:nrow(data)])
+ints <- map(1:nrow(data), ~ seq(t_1[.x], t_2[.x]))
   
 # Set year intervals
-t_1_y <- with(new_data, tapply(1:nrow(new_data), year, min))
-t_2_y <- with(new_data, tapply(1:nrow(new_data), year, max))
+t_1_y <- with(data, tapply(1:nrow(data), year, min))
+t_2_y <- with(data, tapply(1:nrow(data), year, max))
 ints_y <- map(1:length(t_1_y), ~ seq(t_1_y[.x], t_2_y[.x]))
 
+# Set tau & intervals of days
+new_t_1 <- as.vector(newjagsdataBasic$tau1[1:nrow(new_data)])
+new_t_2 <- as.vector(newjagsdataBasic$tau2[1:nrow(new_data)])
+new_ints <- map(1:nrow(new_data), ~ seq(new_t_1[.x], new_t_2[.x]))
+
+# Set year intervals
+new_t_1_y <- with(new_data, tapply(1:nrow(new_data), year, min))
+new_t_2_y <- with(new_data, tapply(1:nrow(new_data), year, max))
+new_ints_y <- map(1:length(new_t_1_y), ~ seq(new_t_1_y[.x], new_t_2_y[.x]))
+
 # Helper functions
-y <- function(x, type) {
-  parms_mean <- switch(type,
-                  base = parms_mean,
-                  low = parms_mean_lo,
-                  high = parms_mean_hi)
+y <- function(x, model, cred) {
+  data <- switch(model,
+                 new = newjagsdataBasic,
+                 old = jagsdataBasic)
+  
+  if (model == "new") {
+    parms_mean <- switch(cred,
+                         base = new_parms_mean,
+                         low = new_parms_mean_lo,
+                         high = new_parms_mean_hi)
+  } else {parms_mean <- switch(cred,
+                               base = parms_mean,
+                               low = parms_mean_lo,
+                               high = parms_mean_hi)}
     
-  parms_mean$g_intcp + parms_mean$log.lambda * newjagsdataBasic$year[x] +
-    parms_mean$c[1] * newjagsdataBasic$daynr[x] + parms_mean$c[2] *
-    newjagsdataBasic$daynr2[x] + parms_mean$c[3] * newjagsdataBasic$daynr[x] *
-    newjagsdataBasic$year[x] + parms_mean$c[4] * newjagsdataBasic$daynr2[x] *
-    newjagsdataBasic$year[x] + parms_mean$b[newjagsdataBasic$loctype[x]] +
-    parms_mean$eps[newjagsdataBasic$plot[x]]
+  parms_mean$g_intcp + parms_mean$log.lambda * data$year[x] +
+    parms_mean$c[1] * data$daynr[x] + parms_mean$c[2] *
+    data$daynr2[x] + parms_mean$c[3] * data$daynr[x] *
+    data$year[x] + parms_mean$c[4] * data$daynr2[x] *
+    data$year[x] + parms_mean$b[data$loctype[x]] +
+    parms_mean$eps[data$plot[x]]
 }
 
 z <- function(x, ...) exp(y(x, ...))
 
 # Calculate predicted biomass
-z_val <- unlist(map(1:nrow(new_data), ~ sum(z(unlist(ints[.x]), "base"))))
+# Old model
+z_val <- unlist(map(1:nrow(data), ~ sum(z(unlist(ints[.x]), "old", "base"))))
 z_val_y <- unlist(map(1:length(ints_y), ~ sum(z_val[unlist(ints_y[.x])])))
 
 # 2.5 % credible interval
-z_val_lo <- unlist(map(1:nrow(new_data), ~ sum(z(unlist(ints[.x]), "low"))))
+z_val_lo <- unlist(map(1:nrow(data), ~ sum(z(unlist(ints[.x]), "old", "low"))))
 z_val_y_lo <- unlist(map(1:length(ints_y), ~ sum(z_val_lo[unlist(ints_y[.x])])))
 
 # 97.5 % credible interval
-z_val_hi <- unlist(map(1:nrow(new_data), ~ sum(z(unlist(ints[.x]), "high"))))
+z_val_hi <- unlist(map(1:nrow(data), ~ sum(z(unlist(ints[.x]), "old", "high"))))
 z_val_y_hi <- unlist(map(1:length(ints_y), ~ sum(z_val_hi[unlist(ints_y[.x])])))
 
+# New model
+new_z_val <- unlist(map(1:nrow(new_data), ~ sum(z(unlist(new_ints[.x]), "new", "base"))))
+new_z_val_y <- unlist(map(1:length(new_ints_y), ~ sum(new_z_val[unlist(new_ints_y[.x])])))
+
+# 2.5 % credible interval
+new_z_val_lo <- unlist(map(1:nrow(new_data), ~ sum(z(unlist(new_ints[.x]), "new", "low"))))
+new_z_val_y_lo <- unlist(map(1:length(new_ints_y), ~ sum(new_z_val_lo[unlist(new_ints_y[.x])])))
+
+# 97.5 % credible interval
+new_z_val_hi <- unlist(map(1:nrow(new_data), ~ sum(z(unlist(new_ints[.x]), "new", "high"))))
+new_z_val_y_hi <- unlist(map(1:length(new_ints_y), ~ sum(new_z_val_hi[unlist(new_ints_y[.x])])))
+
 m_bio_df <- data.frame("year" = 1:26, "m_bio" = NA, "m_bio_lo" = NA,
+                           "m_bio_hi" = NA) # Create initial df
+
+new_m_bio_df <- data.frame("year" = 1:26, "m_bio" = NA, "m_bio_lo" = NA,
                        "m_bio_hi" = NA) # Create initial df
 
 # Fill df; set missing years to previous val
-fill <- function(type) {
-  coln <- switch(type,
+fill <- function(model, cred) {
+  coln <- switch(cred,
                      base = 2,
                      low = 3,
                      high = 4)
   
-  z_val_y <- switch(type,
-                    base = z_val_y,
-                    low = z_val_y_lo,
-                    high = z_val_y_hi)
+  data <- switch(model,
+                 new = new_m_bio_df,
+                 old = m_bio_df)
+  
+  if (model == "new") {
+    z_val_y <- switch(cred,
+                      base = new_z_val_y,
+                      low = new_z_val_y_lo,
+                      high = new_z_val_y_hi)
+    
+  } else {
+    z_val_y <- switch(cred,
+                      base = z_val_y,
+                      low = z_val_y_lo,
+                      high = z_val_y_hi)
+  }
 
-  m_bio_df[1:7, coln] <<- z_val_y[1:7]
-  m_bio_df[9, coln] <<- z_val_y[8]
-  m_bio_df[11:13, coln] <<- z_val_y[9:11]
-  m_bio_df[15:26, coln] <<- z_val_y[12:23]
+  data[1:7, coln] <<- z_val_y[1:7]
+  data[9, coln] <<- z_val_y[8]
+  data[11:13, coln] <<- z_val_y[9:11]
+  data[15:26, coln] <<- z_val_y[12:23]
 
-  m_bio_df[8, coln] <<- m_bio_df[7, coln]
-  m_bio_df[10, coln] <<- m_bio_df[9, coln]
-  m_bio_df[14, coln] <<- m_bio_df[13, coln]
+  data[8, coln] <<- data[7, coln]
+  data[10, coln] <<- data[9, coln]
+  data[14, coln] <<- data[13, coln]
 } # helper function
 
-fill("base")
-fill("low")
-fill("high")
+fill("new", "base")
+fill("new", "low")
+fill("new", "high")
 
-# vr <- function(x) {
-#   unlist(map(x, ~ exp(2 * y(.x) + (parms_mean$sdhat^2)) *
-#                (exp((parms_mean$sdhat^2)) - 1)))
-# }
-# 
-# var <- function(x) {
-#   unlist(map(x, ~ sum(vr(unlist(ints[.x])))))
-# }
-# 
-# m_bio <- function(x) {
-#   z <- unlist(map(x, ~ sum(z(unlist(ints[.x])))))
-#   var <- var(x)
-#   
-#   dnorm(x, mean = z, sd = var)
-# }
-
+fill("old", "base")
+fill("old", "low")
+fill("old", "high")
   
 # ---- Visualization ----
 # Custom palette to match colors from paper
